3736c9047abc6c7e0e872d6fecc391566418440c
itrieditlikethis <albert.fedev@gmail.com>
2026-01-29 17:35:02 +0200

    feat: add text and bg color constants

--- lib/ansi_cli_helper.dart
+++ lib/ansi_cli_helper.dart
@@ -1,1 +1,21 @@
+// Background color constants
+typedef AnsiBackgroundColor = String;
+const AnsiBackgroundColor blackBgColor = '\u001b[40m';
+const AnsiBackgroundColor redBgColor = '\u001b[41m';
+const AnsiBackgroundColor greenBgColor = '\u001b[42m';
+const AnsiBackgroundColor yellowBgColor = '\u001b[43m';
+const AnsiBackgroundColor blueBgColor = '\u001b[44m';
+const AnsiBackgroundColor magentaBgColor = '\u001b[45m';
+const AnsiBackgroundColor cyanBgColor = '\u001b[46m';
+const AnsiBackgroundColor whiteBgColor = '\u001b[47m';
 
+// Text color constants
+typedef AnsiTextColor = String;
+const AnsiTextColor blackTColor = '\u001b[30m';
+const AnsiTextColor redTColor = '\u001b[31m';
+const AnsiTextColor greenTColor = '\u001b[32m';
+const AnsiTextColor yellowTColor = '\u001b[33m';
+const AnsiTextColor blueTColor = '\u001b[34m';
+const AnsiTextColor magentaTColor = '\u001b[35m';
+const AnsiTextColor cyanTColor = '\u001b[36m';
+const AnsiTextColor whiteTColor = '\u001b[37m';
