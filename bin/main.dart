98d2b2a7a1f034269912895eb08511aefc48dd5e
itrieditlikethis <albert.fedev@gmail.com>
2026-02-20 18:32:36 +0200

    feat: add libs to the main

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,10 @@
 import 'package:tetris_cli/tetris_cli.dart' as tetris_cli;
+import 'package:tetris_cli/ansi_cli_helper.dart' as ansi;
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  ansi.reset();
+  ansi.hideCursor();
+
+  tetris_cli.initGame();
+  tetris_cli.start();
+}
